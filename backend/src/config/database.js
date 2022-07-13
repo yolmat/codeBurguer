04abdc743f5111bf22589716fbd6240dc58f0b73
yolmat <mateus.saraiva09@gmail.com>
2022-07-13 10:47:50 -0300

    add config/database

---
+++ backend/src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+  dialect: 'postgres',
+  host: 'localhost',
+  username: 'yolmat',
+  password: '450928',
+  database: 'CodeBurger',
+  define: {
+    timespamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+}
