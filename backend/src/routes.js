7f9ca950ec09125a68b5bdcb1455295500e0ba75
yolmat <mateus.saraiva09@gmail.com>
2022-07-26 04:13:30 -0300

    created products controller

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,4 +1,5 @@
 import { Router } from 'express'
+import ProductController from './app/controllers/ProductController'
 import SessionController from './app/controllers/SessionController'
 
 import UserController from './app/controllers/UserController'
@@ -8,5 +9,7 @@ const routes = new Router()
 routes.post('/users', UserController.store)
 
 routes.post('/sessions', SessionController.store)
+
+routes.post('/products', ProductController.store)
 
 export default routes
