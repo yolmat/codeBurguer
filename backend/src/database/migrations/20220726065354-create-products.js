89e478106facd5619b92dd49c8f510e5b2230be9
yolmat <mateus.saraiva09@gmail.com>
2022-07-26 04:00:24 -0300

    Created migration products

---
+++ backend/src/database/migrations/20220726065354-create-products.js
@@ -0,0 +1,42 @@
+'use strict'
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    return await queryInterface.createTable('products', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false, // essa linha faz com que não possam deixar esse valor em branco na hora de jogar no BQ (banco de dados)
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false, // essa linha faz com que não possam deixar esse valor em branco na hora de jogar no BQ (banco de dados)
+      },
+      price: {
+        type: Sequelize.INTEGER,
+        allowNull: false, // essa linha faz com que não possam deixar esse valor em branco na hora de jogar no BQ (banco de dados)
+      },
+      category: {
+        type: Sequelize.STRING,
+        allowNull: false, // essa linha faz com que não possam deixar esse valor em branco na hora de jogar no BQ (banco de dados)
+      },
+      path: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      created_at: {
+        type: Sequelize.DataTypes.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DataTypes.DATE,
+        allowNull: false,
+      },
+    })
+  },
+
+  down: async (queryInterface) => {
+    return await queryInterface.dropTable('users')
+  },
+}
