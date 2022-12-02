e274954e3d144152eb0cf3dfec87e03cfe808a8d
Mateus <mateus.saraiva09@gmail.com>
2022-12-02 02:59:02 -0300

    Home page and route private

--- frontend/src/routes/routes.js
+++ frontend/src/routes/routes.js
@@ -1,13 +1,16 @@
 import React from 'react'
 import { Switch, Route, BrowserRouter as Router } from 'react-router-dom'
 
+import Home from '../containers/home'
 import Login from '../containers/Login'
 import Register from '../containers/Register'
+import PrivateRoute from './private-route'
 
 function Routers() {
     return (
         <Router>
             <Switch>
+                <PrivateRoute exact component={Home} path="/"></PrivateRoute>
                 <Route component={Login} path="/login"></Route>
                 <Route component={Register} path="/cadastro"></Route>
             </Switch>
