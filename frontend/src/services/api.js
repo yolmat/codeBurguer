17d0265f2eea28d77d6228266a3f9cba41d05c0d
Mateus <mateus.saraiva09@gmail.com>
2022-12-04 05:41:19 -0300

    Token

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -4,4 +4,12 @@ const apiCodeBurguer = axios.create({
     baseURL: 'http://localhost:3001'
 })
 
+apiCodeBurguer.interceptors.request.use(async config => {
+    const userData = await localStorage.getItem('codeburguer:userDate')
+    const token = userData && JSON.parse(userData).token
+    config.headers.authorization = `Bearer: ${token}`
+
+    return config
+})
+
 export default apiCodeBurguer
